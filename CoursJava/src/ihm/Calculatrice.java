c79f77727a884e9968ff910ade723d050521a40a
Klaus <alexandre.mesle@gmail.com>
2014-11-01 02:06:24 +0100

    * Débuggage de la calculatrice.

--- CoursJava/src/ihm/Calculatrice.java
+++ CoursJava/src/ihm/Calculatrice.java
@@ -1,140 +1,139 @@
 package ihm;
 
 import javax.swing.*;
+
 import java.awt.event.*;
 import java.awt.*;
 
 public class Calculatrice extends JFrame
 {
-    final JLabel resultLabel;
-    final JButton[] buttons;
-    
-    private double left = 0;
-    private double right = 0;
-    private char operator;
-    private boolean firstNumber = true;
-	
-    private void addDigit(int i)
-    {
-	if (firstNumber)
-	    {
-		left = 10*left + i;
-		updateScreen(left);
-	    }
-	else
-	    {
-		right = 10*right + i;
-		updateScreen(right);
-	    }
-	System.out.println(i);
-    }    
+	private JLabel resultLabel;
+	private JButton[] buttons;
 
-    private void addOperator(char operator)
-    {
-	System.out.println(operator);
-	if (firstNumber)
-	    firstNumber = false;
-	else
-	    {
-		switch(operator)
-		    {
-		    case '+' : left += right ; break;
-		    case '-' : left -= right ; break;
-		    case '*' : left *= right ; break;
-		    case '/' : left /= right ; break;
-		    case '=' : ; break;
-		    }
-		updateScreen(left);
-		right = 0;
-	    }
-	this.operator = operator;
-    }        
-    
-    public Calculatrice()
-    {
-	setTitle("Calculatrice");
-	setSize(200, 190);
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
-	getContentPane().setLayout(new BorderLayout());
-	setResizable(false);
-	resultLabel = new JLabel("0");
-	JPanel buttonsPanel = new JPanel();
-	getContentPane().add(resultLabel, BorderLayout.NORTH);
-	buttons = new JButton[10 + 4 + 1];
-	buttonsPanel.setLayout(new GridLayout(5, 3));
-	for(int i = 0 ; i < 10 ; i++)
-	    buttons[i] = new JButton("" + i);
-	for(int i = 0 ; i < 10 ; i++)
-	    {
-		final int j = i;
-		buttons[i].addActionListener(new ActionListener()
-		    {
+	private double left = 0;
+	private double right = 0;
+	private char operator;
+	private boolean firstNumber = true;
+
+	private void addDigit(int i)
+	{
+		if (firstNumber)
+		{
+			left = 10 * left + i;
+			updateScreen(left);
+		} 
+		else
+		{
+			right = 10 * right + i;
+			updateScreen(right);
+		}
+		System.out.println(i);
+	}
+
+	private void addOperator(char operator)
+	{
+		System.out.println(operator);
+		if (firstNumber)
+			firstNumber = false;
+		else
+		{
+			switch (this.operator)
+			{
+			case '+':
+				left += right;
+				break;
+			case '-':
+				left -= right;
+				break;
+			case '*':
+				left *= right;
+				break;
+			case '/':
+				left /= right;
+				break;
+			case '=':
+				;
+				break;
+			}
+			updateScreen(left);
+			right = 0;
+		}
+		this.operator = operator;
+	}
+
+	private JPanel getMainPanel()
+	{
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		resultLabel = new JLabel("0");
+		panel.add(resultLabel, BorderLayout.NORTH);
+		panel.add(getButtonsPanel(), BorderLayout.SOUTH);
+		return panel;
+	}
+	
+	
+	private ActionListener addDigitEvent(final int j)
+	{
+		return new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				addDigit(j);
+			}
+		};
+	}
+	
+	private JButton makeOperatorButton(final char operator)
+	{
+		JButton button = new JButton("" + operator);
+		button.addActionListener(new ActionListener()
+		{
 			public void actionPerformed(ActionEvent e)
 			{
-			    addDigit(j);
+				addOperator(operator);
 			}
-		    }
-		    );
-	    }
-	buttons[10] = new JButton("+");
-	buttons[10].addActionListener(new ActionListener()
-	    {
-		public void actionPerformed(ActionEvent e)
-		{
-		    addOperator('+');
-		}
-	    }
-	    );
-	buttons[11] = new JButton("-");
-	buttons[11].addActionListener(new ActionListener()
-	    {
-		public void actionPerformed(ActionEvent e)
-		{
-		    addOperator('-');
-		}
-	    }
-	    );
-	buttons[12] = new JButton("*");
-	buttons[12].addActionListener(new ActionListener()
-	    {
-		public void actionPerformed(ActionEvent e)
-		{
-		    addOperator('*');
-		}
-	    }
-	    );
-	buttons[13] = new JButton("/");
-	buttons[13].addActionListener(new ActionListener()
-	    {
-		public void actionPerformed(ActionEvent e)
-		{
-		    addOperator('/');
-		}
-	    }
-	    );
-	buttons[14] = new JButton("=");
-	buttons[14].addActionListener(new ActionListener()
-	    {
-		public void actionPerformed(ActionEvent e)
-		{
-		    addOperator('=');
-		}
-	    }
-	    );
-	for(int i = 0 ; i < 10 + 4 + 1 ; i++)
-	    buttonsPanel.add(buttons[i]);	
-	getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
-	setVisible(true);
-    }
-    
-    private void updateScreen(double value)
-    {
-	resultLabel.setText((new Double(value)).toString());
-    }
-    
-    public static void main(String[] args)
-    {
-	Calculatrice c = new Calculatrice();
-    }
-}
+		});
+		return button;
+	}
+	
+	private JPanel getButtonsPanel()
+	{
+		JPanel buttonsPanel = new JPanel();
+		buttons = new JButton[10 + 4 + 1];
+		buttonsPanel.setLayout(new GridLayout(5, 3));
+		for (int i = 0; i < 10; i++)
+			buttons[i] = new JButton("" + i);
+		for (int i = 0; i < 10; i++)
+			buttons[i].addActionListener(addDigitEvent(i));
+		buttons[10] = makeOperatorButton('+');
+		buttons[11] = makeOperatorButton('-');
+		buttons[12] = makeOperatorButton('*');
+		buttons[13] = makeOperatorButton('/');
+		buttons[14] = makeOperatorButton('=');
+		for (int i = 0; i < 10 + 4 + 1; i++)
+			buttonsPanel.add(buttons[i]);
+		return buttonsPanel;
 
+	}
+	
+	public Calculatrice()
+	{
+		setTitle("Calculatrice");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
+		setContentPane(getMainPanel());
+		setVisible(true);
+		pack();
+	}
+
+	private void updateScreen(double value)
+	{
+		resultLabel.setText((new Double(value)).toString());
+	}
+
+	public static void main(String[] args)
+	{
+		new Calculatrice();
+	}
+}
