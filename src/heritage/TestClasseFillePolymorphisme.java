35d433cbcf287b47e4ad0400222e1cfcbb8b8a51
klaus <alexandre.mesle@gmail.com>
2017-09-15 11:22:03 +0200

    update line return in TestClasseFillePolymorphisme

--- src/heritage/TestClasseFillePolymorphisme.java
+++ src/heritage/TestClasseFillePolymorphisme.java
@@ -5,7 +5,8 @@ public class TestClasseFillePolymorphisme
 	public static void main(String[] args)
 	{
 		ClasseMere o = new ClasseFille(1, 2);
-		System.out.println("(" + o.getX() + ", " + ((ClasseFille) o).getY()
+		System.out.println("(" + o.getX() + ", " 
+				+ ((ClasseFille) o).getY()
 				+ ")");
 	}
 }
