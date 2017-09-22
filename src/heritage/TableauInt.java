5d375e3fdba75555af98d57963ac30bc07562703
klaus <alexandre.mesle@gmail.com>
2017-09-22 11:10:18 +0200

    heritage.tableauInt passé en ArrayList<>

--- src/heritage/TableauInt.java
+++ src/heritage/TableauInt.java
@@ -1,96 +1,89 @@
 package heritage;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class TableauInt
 {
-	private int[] t;
+	private ArrayList<Integer> t;
 
-	/*-----------------------------------------------*/
-
-	public TableauInt(int taille)
+	public TableauInt()
 	{
-		t = new int[taille];
+		t = new ArrayList<>();
 	}
-
-	/*-----------------------------------------------*/
 
 	public TableauInt(TableauInt other)
 	{
-		t = new int[other.t.length];
-		for (int i = 0; i < t.length; i++)
-			t[i] = other.t[i];
+		t = new ArrayList<>();
+		for (int i = 0 ; i < other.taille() ; i++)
+			t.add(other.get(i));
 	}
 
-	/*-----------------------------------------------*/
+	public int taille()
+	{
+		return t.size();
+	}
 
 	public TableauInt copie()
 	{
 		return new TableauInt(this);
 	}
 
-	/*-----------------------------------------------*/
-
 	public String toString()
 	{
 		String res = "[";
-		if (t.length >= 1)
-			res += t[0];
-		for (int i = 1; i < t.length; i++)
-			res += ", " + t[i];
+		if (taille() >= 1)
+			res += t.get(0);
+		for (int i = 1 ; i < taille() ; i++)
+			res += ", " + t.get(i);
 		res += "]";
 		return res;
 	}
 
-	/*-----------------------------------------------*/
-
 	public int get(int index)
 	{
-		return t[index];
+		return t.get(index);
 	}
-
-	/*-----------------------------------------------*/
 
 	public void set(int index, int value)
 	{
-		t[index] = value;
+		int n = taille();
+		if (index < n)
+			t.set(index, value);
+		if (index == n)
+			t.add(value);
+		if (index < n)
+			System.out.println("Achtung !");
 	}
-
-	/*-----------------------------------------------*/
 
 	public void echange(int i, int j)
 	{
-		int temp = t[i];
-		t[i] = t[j];
-		t[j] = temp;
+		int temp = t.get(i);
+		t.set(i, t.get(j));
+		t.set(j, temp);
 	}
-
-	/*-----------------------------------------------*/
 
 	public void triSelection()
 	{
-		for (int i = 0; i < t.length - 1; i++)
+		for (int i = 0 ; i < taille() - 1 ; i++)
 		{
 			int indiceMin = i;
-			for (int j = i + 1; j < t.length; j++)
-				if (t[indiceMin] > t[j])
+			for (int j = i + 1 ; j < taille() ; j++)
+				if (t.get(indiceMin) > t.get(j))
 					indiceMin = j;
 			echange(i, indiceMin);
 		}
 	}
 
-	/*-----------------------------------------------*/
-
 	public static void main(String[] args)
 	{
-		int n = 10;
+		int n = 10 ;
 		Random r = new Random();
-		TableauInt tab = new TableauInt(n);
+		TableauInt tab = new TableauInt();
 		for (int i = 0; i < n; i++)
 			tab.set(i, r.nextInt() % 100);
 		System.out.println(tab);
 		tab.triSelection();
 		System.out.println(tab);
 	}
-
 }
