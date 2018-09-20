5b20dcb0691c7dccbb707a1c59653bb8715a8c2a
mesle <mesle@PC-SALLE-12.itic.lan>
2018-09-20 16:12:31 +0200

    Correction indices dans NativeArray

--- src/encapsulation/NativeArray.java
+++ src/encapsulation/NativeArray.java
@@ -4,7 +4,7 @@ public class NativeArray
 {
 	public static void main(String[] args)
 	{
-		int taille = (48 - 2) / 3 + 1;
+		int taille = (47 - 2) / 3 + 1;
 		int[] a = new int[taille];
 		for (int value = 2, index = 0; value < 50; value += 3, index++)
 			a[index] = value;
