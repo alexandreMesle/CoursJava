8739181ea0aff2185b37b3b63564a0e7e8fc970e
amesle <amesle@172.16.2.21>
2018-12-04 12:05:43 +0100

    Corrigé Runnable : Lambda expression

--- src/threads/corriges/BinaireAleatoireRunnable.java
+++ src/threads/corriges/BinaireAleatoireRunnable.java
@@ -1,30 +1,10 @@
 package threads.corriges;
 
-public class BinaireAleatoireRunnable implements Runnable
+public class BinaireAleatoireRunnable
 {
-	private int value;
-	private int nbIterations;
-
-	public BinaireAleatoireRunnable(int value, int nbIterations)
-	{
-		this.value = value;
-		this.nbIterations = nbIterations;
-	}
-
-	@Override
-	public void run()
-	{
-		for (int i = 1; i <= nbIterations; i++)
-			System.out.print(value);
-	}
-
 	public static void main(String[] args)
 	{
-		Runnable runnableUn = new BinaireAleatoireRunnable(1, 30);
-		Runnable runnableZero = new BinaireAleatoireRunnable(0, 30);
-		Thread threadUn = new Thread(runnableUn);
-		Thread threadZero = new Thread(runnableZero);
-		threadUn.start();
-		threadZero.start();
+		(new Thread(() -> {for (int i = 1; i <= 30; i++)System.out.print("1");})).start();
+		(new Thread(() -> {for (int i = 1; i <= 30; i++)System.out.print("0");})).start();
 	}
 }
