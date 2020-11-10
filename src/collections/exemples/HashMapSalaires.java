1b677dd3c2e74268199e7b07b729b81da20a1a3e
Le Purifificateur <Le Purifificateur@DESKTOP-H31RRN5>
2020-11-10 11:45:56 +0100

    [Collections] exemple HashMap

--- src/collections/exemples/HashMapSalaires.java
+++ src/collections/exemples/HashMapSalaires.java
@@ -1,13 +1,13 @@
 package collections.exemples;
 
-import java.util.TreeMap;
-import java.util.SortedMap;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapSalaires
 {
 	public static void main(String[] args)
 	{
-		SortedMap<String, Integer> salaires = new TreeMap<>();
+		Map<String, Integer> salaires = new HashMap<>();
 		salaires.put("Raymond", 1000);
 		salaires.put("Marcel", 2000);
 		salaires.put("Ursule", 3000);
