790d20416f515aa9a30664f4647765647f4a7f82
Alex <contact@aexandre-mesle.com>
2021-10-01 14:47:37 +0200

    [Héritage] mise en page de TestClasseFillePolymorphisme

--- src/heritage/TestClasseFillePolymorphisme.java
+++ src/heritage/TestClasseFillePolymorphisme.java
@@ -5,8 +5,7 @@ public class TestClasseFillePolymorphisme
 	public static void main(String[] args)
 	{
 		ClasseMere o = new ClasseFille(1, 2);
-		System.out.println("(" + o.getX() + ", " 
-				+ ((ClasseFille) o).getY()
-				+ ")");
+		System.out.println(o.getX()); 
+		System.out.println(((ClasseFille) o).getY());
 	}
 }
