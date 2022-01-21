a5334b6b133c3403be94023d1edbec1683244525
klaus <alexandre.mesle@gmail.com>
2022-01-21 12:40:51 +0100

    [Ihm] variable locale container

--- src/ihm/PremiersJButtons.java
+++ src/ihm/PremiersJButtons.java
@@ -10,10 +10,11 @@ public class PremiersJButtons
 		JFrame frame = new JFrame();
 		frame.setTitle("My second window !");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new FlowLayout());
-		frame.getContentPane().add(new JButton("my First JButton"));
-		frame.getContentPane().add(new JButton("my Second JButton"));
-		frame.getContentPane().add(new JButton("my Third JButton"));
+		Container container = frame.getContentPane();
+		container.setLayout(new FlowLayout());
+		container.add(new JButton("my First JButton"));
+		container.add(new JButton("my Second JButton"));
+		container.add(new JButton("my Third JButton"));
 		frame.setVisible(true);
 		frame.pack();
 	}
