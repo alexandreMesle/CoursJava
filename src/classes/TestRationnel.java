ef25493bd5c9b6c04be2573b10f8dd15cb7df563
klaus <alexandre.mesle@gmail.com>
2023-09-16 21:10:03 +0200

    [Classes] amélioration de tests Rationnels

--- src/classes/TestRationnel.java
+++ src/classes/TestRationnel.java
@@ -6,12 +6,24 @@ import org.junit.jupiter.api.Test;
 
 class TestRationnel
 {
-
+    private Rationnel cree(int num, int den)
+    {
+        Rationnel r = new Rationnel();
+        r.num = num;
+        r.den = den;
+        return r;
+    }
+    
+    private boolean equals(Rationnel a, Rationnel b)
+    {
+        return a != null && b != null && a.num * b.den == a.den * b.num;
+    }
+    
 	@Test
 	void testToString()
 	{
-		assertEquals("5/64", Rationnel.cree(5, 64).toString(), "Création de 5/64");
-		assertEquals("-5/8", Rationnel.cree(-5, 8).toString(), "Création de -5/8");
+		assertEquals("5/64", cree(5, 64).toString(), "Création de 5/64");
+		assertEquals("-5/8", cree(-5, 8).toString(), "Création de -5/8");
 	}
 
 	@Test
@@ -24,7 +36,7 @@ class TestRationnel
 	@Test
 	void testCopie()
 	{
-		Rationnel r = Rationnel.cree(6, 5), t = r.copie();
+		Rationnel r = cree(6, 5), t = r.copie();
 		assertTrue(r.num == t.num && r.den == t.den, "Copie de 6/5");
 		assertNotSame(r, t, "Copie de 6/5 différente de l'original");
 	}
@@ -32,65 +44,66 @@ class TestRationnel
 	@Test
 	void testOppose()
 	{
-		assertEquals(Rationnel.cree(-4, 5), Rationnel.cree(4, 5).oppose(), "Opposé de 4/5");
-		assertEquals(Rationnel.cree(8, 1), Rationnel.cree(-8, 1).oppose(), "Opposé de -8/1");
+		assertTrue(equals(cree(-4, 5), cree(4, 5).oppose()), "Opposé de 4/5");
+		assertTrue(equals(cree(8, 1), cree(-8, 1).oppose()), "Opposé de -8/1");
 	}
 
 	@Test
 	void testInverse()
 	{
-		assertEquals(Rationnel.cree(4, 5), Rationnel.cree(5, 4).inverse(), "Inverse de 5/4");
-		assertEquals(Rationnel.cree(-11, 5), Rationnel.cree(-5, 11).inverse(), "Inverse de -5/11");
+		assertTrue(equals(cree(4, 5), cree(5, 4).inverse()), "Inverse de 5/4");
+		assertTrue(equals(cree(-11, 5), cree(-5, 11).inverse()), "Inverse de -5/11");
 	}
 
 	@Test
 	void testEstPositif()
 	{
-		assertTrue(Rationnel.cree(4, 5).estPositif(), "4/5 > 0 ?");
-		assertFalse(Rationnel.cree(-4, 5).estPositif(), "-4/5 > 0 ?");
-		assertFalse(Rationnel.cree(4, -5).estPositif(), "4/-5 > 0 ?");
-		assertTrue(Rationnel.cree(-4, -5).estPositif(), "-4/-5 > 0 ?");
+		assertTrue(cree(4, 5).estPositif(), "4/5 > 0 ?");
+		assertFalse(cree(-4, 5).estPositif(), "-4/5 > 0 ?");
+		assertFalse(cree(4, -5).estPositif(), "4/-5 > 0 ?");
+		assertTrue(cree(-4, -5).estPositif(), "-4/-5 > 0 ?");
+		assertFalse(cree(0, 2).estPositif(), "0/2 > 0 ?");
 	}
 
 	@Test
 	void testPlus()
 	{
-		assertEquals(Rationnel.cree(2, 1), Rationnel.cree(1, 2).plus(Rationnel.cree(6, 4)), "1/2 + 6/4 = 2/1");
+		assertTrue(equals(cree(2, 1), cree(1, 2).plus(cree(6, 4))), "1/2 + 6/4 = 2/1");
 	}
 
 	@Test
 	void testMoins()
 	{
-		assertEquals(Rationnel.cree(2, 1), Rationnel.cree(6, 2).moins(Rationnel.cree(1, 1)), "6/2 - 1/1 = 2/1");
+		assertTrue(equals(cree(2, 1), cree(6, 2).moins(cree(1, 1))), "6/2 - 1/1 = 2/1");
 	}
 
 	@Test
 	void testMultiplie()
 	{
-		assertEquals(Rationnel.cree(40, 2), Rationnel.cree(10, 2).multiplie(Rationnel.cree(4, 1)), "10/2 * 4/1 = 40/2");
+		assertTrue(equals(cree(40, 2), cree(10, 2).multiplie(cree(4, 1))), "10/2 * 4/1 = 40/2");
 	}
 
 	@Test
 	void testDivise()
 	{
-		assertEquals(Rationnel.cree(10, 8), Rationnel.cree(10, 2).divise(Rationnel.cree(4, 1)), "(10/2) / (6/4) = 10/8");
+		assertTrue(equals(cree(10, 8), cree(10, 2).divise(cree(4, 1))), "(10/2) / (6/4) = 10/8");
 	}
 
 	@Test
 	void testEquals()
 	{
-		assertTrue(Rationnel.cree(6, 5).equals(Rationnel.cree(6, 5)), "6/5 == 6/5");
-		assertFalse(Rationnel.cree(6, 5).equals(Rationnel.cree(6, 4)), "6/5 == 6/4");
-		assertTrue(Rationnel.cree(2, 1).equals(Rationnel.cree(2, 1)), "2/1 == 2/1");
-		assertTrue(Rationnel.cree(20, 1).equals(Rationnel.cree(20, 1)), "20/1 == 20/1");
+		assertTrue(cree(6, 5).equals(cree(6, 5)), "6/5 == 6/5");
+		assertFalse(cree(6, 5).equals(cree(6, 4)), "6/5 == 6/4");
+		assertTrue(cree(2, 1).equals(cree(2, 1)), "2/1 == 2/1");
+		assertTrue(cree(20, 1).equals(cree(20, 1)), "20/1 == 20/1");
 	}
 
 	@Test
 	void testCompareTo()
 	{
-		assertTrue(Rationnel.cree(5, 7).compareTo(Rationnel.cree(10, 8)) < 0, "5/7 > 10/8");
-		assertTrue(Rationnel.cree(5, 7).compareTo(Rationnel.cree(-10, 8)) > 0, "5/7 > -10/8");
-		assertTrue(Rationnel.cree(-5, 7).compareTo(Rationnel.cree(-5, 7)) == 0, "-5/7 > -5/7");
+		assertTrue(cree(5, 7).compareTo(cree(10, 8)) < 0, "5/7 > 10/8 doit retourner un nombre négatif");
+		assertTrue(cree(5, 7).compareTo(cree(-10, 8)) > 0, "5/7 > -10/8 doit retourner un nombre positif");
+		assertTrue(cree(-5, 7).compareTo(cree(-5, 7)) == 0, "-5/7 > -5/7 doit retourner zéro");
 	}
 	
 	@Test 
@@ -99,5 +112,4 @@ class TestRationnel
 		assertEquals(1, Rationnel.pgcd(13, 21), "pgcd(13, 21)");
 		assertEquals(5, Rationnel.pgcd(35, 40), "pgcd(35, 40)");
 	}
-
 }
